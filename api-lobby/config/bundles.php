684e66e3fae3a30f8c42055a7928d52c63abd434
Ahmed Kooli <kooliahmd@gmail.com>
2019-07-08 11:54:43 +0200

    lobby

--- api-lobby/config/bundles.php
+++ api-lobby/config/bundles.php
@@ -3,10 +3,11 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
-    SnakeTn\JwtSecurityBundle\ApiBundle::class => ['dev' => true],
     Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    SnakeTn\ApiBundle\ApiBundle::class => ['all' => true],
+    SnakeTn\JwtSecurityBundle\JwtSecurityBundle::class => ['all' => true],
 ];
