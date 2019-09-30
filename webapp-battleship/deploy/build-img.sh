a7cae0d98bd299c27672b2b48428361dfbf4e080
Ahmed Kooli <kooliahmd@gmail.com>
2019-09-30 13:48:01 +0200

    devops

--- webapp-battleship/deploy/build-img.sh
+++ webapp-battleship/deploy/build-img.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 set -o allexport && source .env && set +o allexport
 cat key-file.json | docker login -u _json_key --password-stdin gcr.io
 docker build -f docker/nginx/Dockerfile ../ -t gcr.io/$GCLOUD_PROJECT_ID/webapp-nginx:$CI_COMMIT_SHA
--- webapp-battleship/public/Config.js
+++
@@ -1,8 +0,0 @@
-
-const urlParams = new URLSearchParams(window.location.search);
-const token = urlParams.get('access_token');
-const game = urlParams.get('game');
-
-config = {
-    ws_url: "ws://localhost:8080?access_token="+token+"&game="+game
-}
