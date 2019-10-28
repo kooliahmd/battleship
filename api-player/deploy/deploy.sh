6313720c91399898def3f26960d29da724a66e9f
Ahmed Kooli <kooliahmd@gmail.com>
2019-10-28 17:13:07 +0100

    add DNS configs

--- api-player/deploy/deploy.sh
+++ api-player/deploy/deploy.sh
@@ -4,6 +4,7 @@ docker run  --env-file .env cloud-sdk sh -c \
    'sed -i "s/CI_COMMIT_SHA/$CI_COMMIT_SHA/g" /kubernetes/* &&
     sed -i "s/GCLOUD_PROJECT_ID/$GCLOUD_PROJECT_ID/g" /kubernetes/* &&
     sed -i "s/APP_NAME/$APP_NAME/g" /kubernetes/* &&
+    sed -i "s/APP_IP/$APP_IP/g" /kubernetes/* &&
 
     gcloud auth activate-service-account --key-file /key-file.json &&
     gcloud container clusters get-credentials --region europe-west4-a battleship &&
